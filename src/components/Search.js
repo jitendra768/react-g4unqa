856a7aeeaba964c9f6c819cfeb3daf47d379ae3a
jitendra768 <jackjitendra768@gmail.com>
2024-04-30 01:04:49 +0530

    some

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,13 +2,16 @@ import React, { useState } from 'react';
 
 const Search = () => {
   const [list, setSearch] = useState(['Apple', 'Kiwi', 'cherry']);
+  const[searchText,setSearchText] = useState("");
+
+  const searchFilter = list.filter((item)=>item.toLowerCase().includes(searchText.toLowerCase()))
   return (
     <>
       <div>
-        <input placholder="type to search" />
+        <input placholder="type to search" onChange={(e)=>setSearchText(e.target.value)}/>
 
         <ul>
-          {list.map((item) => (
+          {searchFilter.map((item) => (
             <li key={item}>{item}</li>
           ))}
         </ul>
